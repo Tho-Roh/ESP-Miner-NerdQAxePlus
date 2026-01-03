402115a8c78ab61e89d7f076f8b68421345e54fa
Tho-Roh <github@thomasrohde.eu>
2026-01-03 23:59:27 +0100

    Add virtual methods to NerdOctaxePlus class

--- main/boards/nerdoctaxeplus.h
+++ main/boards/nerdoctaxeplus.h
@@ -5,4 +5,7 @@
 class NerdOctaxePlus : public NerdQaxePlus {
   public:
     NerdOctaxePlus();
+virtual float getVRTemp() override;
+virtual bool initBoard() override;
+virtual void requestChipTemps() override;
 };
