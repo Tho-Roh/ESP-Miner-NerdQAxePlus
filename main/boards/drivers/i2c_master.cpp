742a98f1c0684d2e605bf8d95d0db9f03feebec8
Tho-Roh <github@thomasrohde.eu>
2026-01-07 02:12:24 +0100

    Update i2c_master.cpp

--- main/boards/drivers/i2c_master.cpp
+++ main/boards/drivers/i2c_master.cpp
@@ -1,18 +1,13 @@
 #include "i2c_master.h"
 
-#define I2C_MASTER_SCL_IO 43        /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO 44        /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_SCL_IO 43
+#define I2C_MASTER_SDA_IO 44
+#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_TX_BUF_DISABLE 0
+#define I2C_MASTER_RX_BUF_DISABLE 0
 
-/**
- * @brief i2c master initialization
- */
 esp_err_t i2c_master_init(void)
 {
-    i2c_port_t i2c_master_port = (i2c_port_t) I2C_MASTER_NUM;
-
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -24,49 +19,58 @@ esp_err_t i2c_master_init(void)
         }
     };
 
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_NUM, &conf));
+    return i2c_driver_install(I2C_MASTER_NUM,
+                              conf.mode,
+                              I2C_MASTER_RX_BUF_DISABLE,
+                              I2C_MASTER_TX_BUF_DISABLE,
+                              0);
 }
 
-/**
- * @brief i2c master delete
- */
 esp_err_t i2c_master_delete(void)
 {
     return i2c_driver_delete(I2C_MASTER_NUM);
 }
 
-/**
- * @brief Read a sequence of I2C bytes
- */
-esp_err_t i2c_master_register_read(uint8_t device_address, uint8_t reg_addr, uint8_t *data, size_t len)
+// -----------------------------------------------------------------------------
+// Port-aware register access
+// -----------------------------------------------------------------------------
+
+esp_err_t i2c_master_register_read_port(i2c_port_t port,
+                                        uint8_t device_address,
+                                        uint8_t reg_addr,
+                                        uint8_t *data,
+                                        size_t len)
 {
-    return i2c_master_write_read_device(I2C_MASTER_NUM, device_address, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_TICKS);
+    return i2c_master_write_read_device(
+        port,
+        device_address,
+        &reg_addr,
+        1,
+        data,
+        len,
+        I2C_MASTER_TIMEOUT_TICKS);
 }
 
-/**
- * @brief Write a byte to a I2C register
- */
-esp_err_t i2c_master_register_write_byte(uint8_t device_address, uint8_t reg_addr, uint8_t data)
+esp_err_t i2c_master_register_write_port(i2c_port_t port,
+                                         uint8_t device_address,
+                                         uint8_t reg_addr,
+                                         const uint8_t *data,
+                                         size_t len)
 {
-    int ret;
-    uint8_t write_buf[2] = {reg_addr, data};
+    uint8_t buf[3];
 
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, device_address, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_TICKS);
+    if (len > 2)
+        return ESP_ERR_NOT_SUPPORTED;
 
-    return ret;
-}
+    buf[0] = reg_addr;
+    for (size_t i = 0; i < len; i++)
+        buf[i + 1] = data[i];
 
-/**
- * @brief Write a word to a I2C register
- */
-esp_err_t i2c_master_register_write_word(uint8_t device_address, uint8_t reg_addr, uint16_t data)
-{
-    int ret;
-    uint8_t write_buf[3] = {reg_addr, (uint8_t) ((data >> 8) & 0xFF), (uint8_t) (data & 0xFF)};
-
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, device_address, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_TICKS);
-
-    return ret;
+    return i2c_master_write_to_device(
+        port,
+        device_address,
+        buf,
+        len + 1,
+        I2C_MASTER_TIMEOUT_TICKS);
 }
