3b31b6b48fbe7d8120f7809e621ace82ae0478d2
Tho-Roh <github@thomasrohde.eu>
2026-01-20 11:08:37 +0100

    Update nerdhaxegamma.cpp

--- main/boards/nerdhaxegamma.cpp
+++ main/boards/nerdhaxegamma.cpp
@@ -45,7 +45,7 @@ NerdHaxeGamma::NerdHaxeGamma() : NerdQaxePlus2() {
 bool NerdHaxeGamma::initBoard() {
     bool ret = NerdQaxePlus2::initBoard();
 
-static TMP468 tmp468(TMP468_ADDR, m_asicCount);
+static TMP468 tmp468(TMP468_ADDR, 8);
 
     if (tmp468.init() == ESP_OK) {
         ESP_LOGI(TAG, "TMP468 detected");
